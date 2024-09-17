1c10bb5c94d743495da40d30c57f8c4dfe1db334
Prajwal Yadav B R <78904638+prajwalbr7@users.noreply.github.com>
2024-09-17 09:56:45 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+import { Habitat } from "./Components/Habitat";
+export default function App() {
+  return (
+    <div className="App">
+      <Habitat />
+    </div>
+  );
+}
---
+++ src/Components/Habitat/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import "./index.css";
+
+class Habitat extends Component {
+  render() {
+    return (
+      <div className="">
+        <h1 className="">hello</h1>
+      </div>
+    );
+  }
+}
+export default Habitat;
